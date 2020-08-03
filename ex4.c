f14d8243714b63825739d1ee62f93e564ce51470
s1270218 <69070861+s1270218@users.noreply.github.com>
2020-08-03 09:38:30 +0900

    HoT: Initial revision

---
+++ ex4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+ int i,n,head=0,tail=0;
+ printf("Tossing a coin...\n");
+
+ for(i=0; i<3; i++){
+     n = rand();
+     if(n%2==0){
+         printf("Round %d; Head\n",i+1);
+         head++;
+     }
+     else{
+         printf("Round %d; Tail\n",i+1);
+         tail++;
+     }
+ }    
+ printf("Heads: %d, Tails: %d\n",head,tail);
+ return 0;
+}
